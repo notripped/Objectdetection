24c2c5677f7c6e57918ec248d8e57ce81778cfa5
Ravi Kumar <142670036+notripped@users.noreply.github.com>
2024-12-25 12:19:41 +0530

    Add files via upload

---
+++ webcam/Webcamyolo.py
@@ -0,0 +1,42 @@
+from streamlit import success
+from ultralytics import YOLO
+import cv2
+import math
+import cvzone
+# cap=cv2.VideoCapture(0) for webcam
+cap=cv2.VideoCapture(r"C:\Users\ravik\Downloads\bikes.mp4")
+
+cap.set(3,640)
+cap.set(4,480)
+model=YOLO('../Weights/yolov8n.pt')
+classnames=["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+"traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+"dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+"handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+"baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+"fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+"carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+"diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+"microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+"teddy bear", "hair drier", "toothbrush"]
+# print(len(classnames))
+while True:
+    success,img=cap.read()
+    # cv2.imshow('Video',img)
+    results=model(img,show=True)
+    for r in results:
+        boxes=r.boxes
+        for box in boxes:
+            #Bounding box
+            x1,y1,x2,y2=box.xyxy[0]
+            x1, y1, x2, y2=int(x1),int(y1),int(x2),int(y2)
+            w,h=x2-x1,y2-y1
+            cvzone.cornerRect(img,(x1,y1,w,h))
+            #Print confidence value
+            conf=math.ceil((box.conf[0]*100))/100
+            #Print class name
+            cls=int(box.cls[0])
+            cvzone.putTextRect(img, f'{classnames[cls]} {conf}', (max(0,x1), max(35,y1)),scale=1,thickness=1)
+
+    cv2.imshow('Video', img)
+    cv2.waitKey(1)
